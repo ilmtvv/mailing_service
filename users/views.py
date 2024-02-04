0e71b548b6bca827e528a3058234146fa12291d0
ilmtvv <napalkah@gmail.com>
2024-02-05 02:31:48 +0300

    create_users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
-from users.forms import UserForm
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
-    form_class = UserForm
