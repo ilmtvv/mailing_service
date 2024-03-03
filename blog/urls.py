6782af7b4325c085a019caa36b5cfe86ad53835d
ilmtvv <napalkah@gmail.com>
2024-03-03 23:37:40 +0300

    update_blog_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
+from blog.views import BlogUnitDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    #path('<int:pk>', BlogUnitDetailView.as_view(), name='detail unit'),
+    path('<int:pk>/', BlogUnitDetailView.as_view(), name='detail_unit'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from blog.models import BlogUnit
+
+
+class BlogUnitDetailView(DetailView):
+    model = BlogUnit
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+
+        return self.object
