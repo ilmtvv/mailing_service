f7af495ba6008673e264be75e9d6d830f2598024
ilmtvv <napalkah@gmail.com>
2024-03-03 11:42:11 +0300

    update_home

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 python manage.py pre_mailing --pk_mailing=18
+{% if object.mailing_status == 0 %}
+        <td><span class="badge rounded-pill text-bg-warning">CREATED</span></td>
+        {% elif object.mailing_status == 1 %}
+        <td><span class="badge rounded-pill text-bg-secondary">ACTIVATING</span></td>
+        {% elif object.mailing_status == 2 %}
+        <td><span class="badge rounded-pill text-bg-danger">STOP</span></td>
+        {% else %}
+        <td><span class="badge rounded-pill text-bg-success">SUCCESS</span></td>
+        {% endif %}
--- home/views.py
+++ home/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
+from blog.models import BlogUnit
+from clients.models import Client
+from mailings.models import Mailing
 
-def home_view(request):
-    return render(request, 'home/home_list.html')
+
+def home_view(request): # как будто бы дофига запросов в базу данных,
+    # но онож кешироваться будет, вот если бы как то разом за один заход вытянуть все
+    # как то это можно было сделать ??
+    queryset = BlogUnit.objects.all().order_by('?')
+    blog_units_list = queryset[:3]
+
+    if request.user.is_authenticated:
+        queryset = Mailing.objects.all().filter(users=request.user)
+        mailings_all = queryset.count()
+
+        mailing_activate = queryset.filter(mailing_status=1).count()
+        queryset = Client.objects.all().filter(users=request.user)
+        count_unique_clients = queryset.count()
+
+        context = {
+            'mailings_all': mailings_all,
+            'mailing_activate': mailing_activate,
+            'count_unique_clients': count_unique_clients,
+            'blog_units_list': blog_units_list,
+
+        }
+    else:
+        context = {
+            'blog_units_list': blog_units_list,
+        }
+    print(blog_units_list)
+    return render(request, 'home/home_list.html', context)
